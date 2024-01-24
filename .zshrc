2e7d7f98c8b77745157ebd24a121eaeca8bbe5a5
Genograche <103960025+Genograche@users.noreply.github.com>
2024-01-24 21:13:16 +0530

    Add files via upload

---
+++ .zshrc
@@ -0,0 +1,22 @@
+# Lines configured by zsh-newuser-install
+HISTFILE=~/.histfile
+HISTSIZE=1000
+SAVEHIST=1000
+bindkey -e
+# End of lines configured by zsh-newuser-install
+# The following lines were added by compinstall
+zstyle :compinstall filename '/home/genograche/.zshrc'
+
+autoload -Uz compinit
+compinit
+# End of lines added by compinstall
+
+source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+eval "$(starship init zsh)"
+
+#alias
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+alias upgrade='sudo pacman -Syyu'
+alias mirror='reflector --latest 6 --sort rate --save /etc/pacman.d/mirrorlist'
---
+++ mysetup.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+#### Check for yay ####
+ISYAY=/sbin/yay
+if [ -f "$ISYAY" ]; then 
+    echo -e "$COK - yay was located, moving on."
+    yay -Suy
+else 
+    echo -e "$CWR - Yay was NOT located"
+    read -n1 -rep $'[\e[1;33mACTION\e[0m] - Would you like to install yay (y,n) ' INSTYAY
+    if [[ $INSTYAY == "Y" || $INSTYAY == "y" ]]; then
+        git clone https://aur.archlinux.org/yay.git &>> $INSTLOG
+        cd yay
+        makepkg -si --noconfirm &>> ../$INSTLOG
+        cd ..
+        
+    else
+        echo -e "$CER - Yay is required for this script, now exiting"
+        exit
+    fi
+fi
+
+### Install all of the above pacakges ####
+read -n1 -rep 'Would you like to install the packages? (y,n)' INST
+if [[ $INST == "Y" || $INST == "y" ]]; then
+    yay -R --noconfirm swaylock waybar
+    yay -S --noconfirm hyprland-git polkit-gnome ffmpeg neovim viewnior \
+    rofi-lbonn-wayland pavucontrol thunar starship cliphist wl-clipboard \
+    wf-recorder swww waypaper grimblast-git ffmpegthumbnailer tumbler gvfs \
+    playerctl noise-suppression-for-voice file-roller thunar-archive-plugin \
+    thunar-media-tags-plugin kitty thunar-volman gvfs-mtp \
+    waybar-git wlogout swaylock-effects pamixer     \
+    nwg-look-bin dunst ttf-firacode-nerd noto-fonts \
+    noto-fonts-emoji ttf-nerd-fonts-symbols-common otf-firamono-nerd \
+    brightnessctl hyprpicker-git pipewire lib32-pipewire wireplumber \
+    pipewire-audio pipewire-pulse pipewire-alsa pipewire-jack \
+    lib32-pipewire-jack xdg-user-dirs xdg-desktop-portal-hyprland \
+    
+fi
+### making directory ###
+
+xdg-user-dirs-update
+
+### Copy Config Files ###
+read -n1 -rep 'Would you like to copy config files? (y,n)' CFG
+if [[ $CFG == "Y" || $CFG == "y" ]]; then
+    echo -e "Copying config files...\n"
+    cp -R config/dunst ~/.config/
+    cp -R config/hypr ~/.config/
+    cp -R config/kitty ~/.config/
+    cp -R config/neofetch ~/.config/
+    #cp -R config/pipewire ~/.config/
+    cp -R config/rofi ~/.config/
+    cp -R config/swaylock ~/.config/
+    cp -R config/waybar ~/.config/
+    cp -R config/wlogout ~/.config/
+    cp -R config/xfce4 ~/.config/
+
+   ## mkdir -p ~/Pictures/wallpaper
+    cp -R ./wallpapers ~/Pictures/
+    
+    # Set some files as exacutable 
+    chmod +x ~/.config/hypr/xdg-portal-hyprland
+    chmod +x ~/.config/waybar/scripts/*
+fi
+
+### Script is done ###
+echo -e "Script had completed.\n"
+echo -e "You can start Hyprland by typing Hyprland (note the capital H).\n"
+read -n1 -rep 'Would you like to start Hyprland now? (y,n)' HYP
+if [[ $HYP == "Y" || $HYP == "y" ]]; then
+    exec Hyprland
+else
+    exit
+fi
+
