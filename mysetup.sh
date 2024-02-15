5ce54496afa9423751e671b5a0c2e8a49709ef59
Genograche <genograche@gmail.com>
2024-02-15 17:07:45 +0530

    small debugging

--- mysetup.sh
+++ mysetup.sh
@@ -21,11 +21,12 @@ else
 fi
 
 ### Install all of the above pacakges ####
+# slurp needed for wlprop?
 read -n1 -rep 'Would you like to install the packages? (y,n)' INST
 if [[ $INST == "Y" || $INST == "y" ]]; then
     yay -S hyprland polkit-kde-agent gnome-keyring seahorse gnome-system-monitor \
     ffmpeg neovim viewnior rofi-lbonn-wayland pavucontrol thunar galculator \
-    starship cliphist wl-clipboard wf-recorder swww waypaper slurp grimblast-git \
+    starship cliphist wl-clipboard wf-recorder swww waypaper grimblast-git \
     ffmpegthumbnailer tumbler gvfs playerctl noise-suppression-for-voice \
     xarchiver thunar-archive-plugin thunar-media-tags-plugin kitty alacritty \
     thunar-volman gvfs-mtp waybar wlogout swaync swaylock-effects pamixer \
